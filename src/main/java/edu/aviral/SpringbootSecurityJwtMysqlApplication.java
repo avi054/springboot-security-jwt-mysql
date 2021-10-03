c6a45afbdd01fe4c21ca0cde272f7993338a50ed
Aviral Agarwal <aviralagarwal.2013@gmail.com>
2021-10-03 20:38:52 +0530

    Initial commit

---
+++ src/main/java/edu/aviral/SpringbootSecurityJwtMysqlApplication.java
@@ -0,0 +1,13 @@
+package edu.aviral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityJwtMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/aviral/SpringbootSecurityJwtMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aviral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityJwtMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
